526b363a9ab9ab11e1d338b295d46b066d9c6c62
Rafał Bobka <pl.everrarder@o2.pl>
2021-05-28 14:42:00 +0200

    Lab 12

--- app/config.js
+++ app/config.js
@@ -3,5 +3,4 @@ const config = {
   databaseUrl: process.env.MONGODB_URI || 'mongodb+srv://pwsz-user:pwsz-pass@cluster0.thbeg.mongodb.net/pwsz-db?retryWrites=true&w=majority',
   JwtSecret: process.env.JWT_SECRET || 'secret'
 };
-
 export default config;
